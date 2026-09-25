2b6d52883b5f6f6fdbfdba2f93bf5ba3d9a256a0
Gustavo Baiao <gg.baiao@gmail.com>
2026-09-25 12:40:54 -0300

    feat - ajuste atibutos

--- src/main/java/br/com/provasmart/api/dto/response/user/UserResponseDTO.java
+++ src/main/java/br/com/provasmart/api/dto/response/user/UserResponseDTO.java
@@ -25,10 +25,10 @@ public record UserResponseDTO(
         boolean active,
 
         @Schema(description = "Indica se o usuário solicitou a exclusão", example = "true")
-        boolean deletedRequested,
+        boolean deletionRequested,
 
         @Schema(description = "Data da solicitação de exclusão do usuário", example = "2023-01-01T00:00:00")
-        LocalDateTime deletedRequestedAt,
+        LocalDateTime deletionRequestedAt,
 
         @Schema(description = "Versão dos Termos de Uso aceita pelo usuário")
         String termsVersion,
